3cb0b3d53305a0a4f2bead0fcd4e1c7f72a2377d
angelisk <gusangelis90@gmail.com>
2022-06-13 13:01:19 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
+const { engine } = require("express-handlebars");
+const connectDB = require("./config/db");
+
+// Initialize variables Start
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Initialize variables End
+
+// Load Configuration Start
+dotenv.config({ path: ".env" });
+// Load Configuration End
+
+// Passport Configuration Start
+require("./config/passport")(passport);
+// Passport Configuration End
+
+// Sessions Start
+app.use(
+  session({
+    //Usage
+    secret: "crazy-monkey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+// Sessions End
+
+// Passport Middleware Start
+app.use(passport.initialize());
+app.use(passport.session());
+// Passport Middleware End
+
+// Connect Database Start
+connectDB();
+// Connect Database End
+
+// Logging In Start
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+// Logging In End
+
+// Handlebars Start
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: "views/layouts/",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+// Handlebars End
+
+// Static Folder Start
+app.use(express.static(path.join(__dirname, "public")));
+// Static Folder End
+
+// Routes Start
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+// Routes End
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../.env" });
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, options);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+    console.log("Connection Status: " + mongoose.connection.readyState);
+  } catch (error) {
+    console.error("Failed to connect: " + error);
+    console.log("Connection Status: " + mongoose.connection.readyState);
+  }
+};
+module.exports = connectDB;
